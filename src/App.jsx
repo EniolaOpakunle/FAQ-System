8d724e694975116815cfb02c65ca23a8a043e12b
unknown <opakunleeniolail2019@gmail.com>
2024-06-24 13:05:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import FAQ from './pages/FAQ'
+
+function App() {
+
+  return (
+    <>
+    <FAQ/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/FAQ.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FAQ() {
+  return (
+    <div>
+        <div>
+            <p>FAQs</p>
+            
+        </div>
+    </div>
+  )
+}
+
+export default FAQ
